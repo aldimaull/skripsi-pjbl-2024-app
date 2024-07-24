bdca82a40a99a1c0828ad8f8f50dab6161fee016
Aldi Maulana <aldimaul@upi.edu>
2024-07-24 13:53:09 +0700

    fix utils data

--- src/app/materi/page.tsx
+++ src/app/materi/page.tsx
@@ -1,28 +1,29 @@
 import React from "react";
-import { ListMateri } from "@/utils/data";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
+import { db } from "@/lib/db";
 import NoSession from "@/components/error/NoSession";
 
 const Materi = async () => {
   const session = await getServerSession(authOptions);
+  const materiList = await db.materiList.findMany();
 
   if (session?.user) {
     return (
       <>
         <h1 className="mb-2 lg:mb-4">Daftar Materi</h1>
         <div className="lg:grid grid-cols-2 lg:gap-4">
-          {ListMateri.map((materi, index) => (
+          {materiList.map((materi, index) => (
             <div
               key={index}
               className="my-4 p-4 lg:my-0 bg-secondary rounded-md space-y-2"
             >
-              <h2>{materi.namaMateri}</h2>
-              <p className="text-sm">Deskripsi materi: {materi.deskripsi}</p>
+              <h2>{materi.name}</h2>
+              <p className="text-sm">Deskripsi materi: {materi.description}</p>
               <div>
-                <Link href={materi.link}>
+                <Link href={`/materi/${materi.id}`}>
                   <Button>Lihat Materi</Button>
                 </Link>
               </div>
--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,31 +1,32 @@
 import React from "react";
-import { ListProject } from "@/utils/data";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
+import { db } from "@/lib/db";
 import NoSession from "@/components/error/NoSession";
 
 const Project = async () => {
   const session = await getServerSession(authOptions);
+  const projectList = await db.projectList.findMany();
 
   if (session?.user) {
     return (
       <>
         <h1 className="mb-2 lg:mb-4">Daftar Project Anda</h1>
         <div className="lg:grid grid-cols-2 lg:gap-4">
-          {ListProject.map((project, index) => (
+          {projectList.map((project, index) => (
             <div
               key={index}
               className="my-4 p-4 lg:my-0 bg-secondary rounded-md space-y-2"
             >
-              <h2>{project.namaProject}</h2>
-              <p>{project.deskripsi}</p>
+              <h2>{project.name}</h2>
+              <p>{project.description}</p>
               <div>
-                <Link href={project.link}>
+                <Link href={`/project/${project.id}`}>
                   <Button>Lihat Project</Button>
                 </Link>
-                <p>Tenggat waktu: {project.tenggat?.toString()}</p>
+                <p>Tenggat waktu: {project.deadline.toLocaleDateString()}</p>
               </div>
             </div>
           ))}
