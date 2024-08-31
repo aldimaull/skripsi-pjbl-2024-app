f0c96a3b8f6e19a0770376ca1e2a5fb8cd17a8af
Aldi Maulana <aldimaul@upi.edu>
2024-08-31 07:51:18 +0700

    fix guru page

--- src/app/(dashboard)/guru/page.tsx
+++ src/app/(dashboard)/guru/page.tsx
@@ -6,25 +6,31 @@ import {
   Project,
   Assessment,
 } from "@/components/(guru)/CardDashboardGuru";
+import NoSession from "@/components/error/NoSession";
 
 const page = async () => {
   const session = await getServerSession(authOptions);
-  return (
-    <div>
-      <h1 className="font-serif tracking-wide text-primary">Dashboard Guru</h1>
-      <h2>
-        Halo, selamat datang kembali{" "}
-        <span className="nama-siswa text-primary font-bold">
-          {session?.user.name}
-        </span>
-        !
-      </h2>
-      <TableProjects />
-      <Project />
-      <Assessment />
-      <Materi />
-    </div>
-  );
+  if (session?.user) {
+    return (
+      <div>
+        <h1 className="font-serif tracking-wide text-primary">
+          Dashboard Guru
+        </h1>
+        <h2>
+          Halo, selamat datang kembali{" "}
+          <span className="nama-siswa text-primary font-bold">
+            {session?.user.name}
+          </span>
+          !
+        </h2>
+        <TableProjects />
+        <Project />
+        <Assessment />
+        <Materi />
+      </div>
+    );
+  }
+  return <NoSession />;
 };
 
 export default page;
