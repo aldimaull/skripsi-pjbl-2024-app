b3f28cfb0cc94430475b08adffa72e7bdc3e22f8
Aldi Maulana <aldimaul@upi.edu>
2024-10-09 08:25:01 +0700

    fix rencana in namaProject

--- src/components/projects/palindrom.tsx
+++ src/components/projects/palindrom.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import CodeEditor from "../CodeEditor";
 
 export default function ClientComponentExample({
@@ -9,14 +10,37 @@ export default function ClientComponentExample({
   userId: string;
   projectId: number;
 }) {
+    const [rencana, setRencana] = useState<any>();
+
+    useEffect(() => {
+        const fetchData = async () => {
+          const response = await fetch(
+            `/api/saveCode?projectId=${projectId}&userId=${userId}`,
+            {
+              method: "GET",
+            }
+          );
+
+          if (response.ok) {
+            const data = await response.json();
+            if (data.data.rencana) {
+              setRencana(data.data.rencana);
+            }
+          } else {
+            console.error("Failed to fetch code");
+          }
+        };
+        fetchData();
+      }, [projectId, userId]);
+
+      console.log(rencana);
+
   return (
     <div className="space-y-4">
       <CodeEditor id="1" userId={userId} projectId={projectId}>
-        <p>
-          Pertama, tentukan suhu awal yang ingin kamu konversi. Pilih empat
-          variabel, satu untuk menyimpan suhu dalam Celsius dan tiga sisanya
-          lagi untuk menyimpan suhu dalam Fahrenheit, Reamur, dan Kelvin.
-        </p>
+        <article
+        dangerouslySetInnerHTML={{__html: rencana}}
+        />
       </CodeEditor>
     </div>
   );
